6a8db88f5c34573adba1ef0065dcec17ba13baee
smritichopra <chopra.smriti17@gmail.com>
2024-07-12 14:26:55 -0400

    adding the files to repository

---
+++ cdk-woocommerce/test/cdk-woocommerce.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWoocommerce from '../lib/cdk-woocommerce-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-woocommerce-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWoocommerce.CdkWoocommerceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
